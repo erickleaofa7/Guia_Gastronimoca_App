1d57f7f8eb88d2716f0b78045855a598b61fbc87
jasson5p <jasson11@live.com>
2020-07-18 18:58:57 -0500

    CarpetaChile

---
+++ app/src/main/java/com/erick/guia_gastronimoca_app/Chile/Layout_Arroz.java
@@ -0,0 +1,14 @@
+package com.erick.guia_gastronimoca_app.Chile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Layout_Arroz extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_layout__arroz);
+    }
+}
